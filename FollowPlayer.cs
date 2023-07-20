4c9f7f70185aa612d8d412af8d0c8fa414189f20
Zhila <zshariat08@gmail.com>
2023-07-20 05:30:23 +0400

    Add files via upload
    Added camera view switcher when spacebar is pressed

--- FollowPlayer.cs
+++ FollowPlayer.cs
@@ -7,6 +7,7 @@ public class FollowPlayer : MonoBehaviour
 
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+    private bool keySwitch = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,24 @@ public class FollowPlayer : MonoBehaviour
     void LateUpdate()
     {
 
+        
+        //Switch camera views on spacebar press
+       
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            keySwitch = !keySwitch;
+            
+        }
+
+        if (keySwitch) { offset = new Vector3(0, 5, 0); }
+        else { offset = new Vector3(0, 5, -7); }
+
         transform.position = player.transform.position + offset;
 
 
     }
+
+
+
+
+
 }
